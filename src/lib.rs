1ab7a7f31ac5b89a34743d75c48353feb61d0b2a
Vadim Dyadkin <dyadkin@3lp.cx>
2021-02-12 16:15:27 +0100

    MPConfig struct with default

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,58 @@
+pub const MP_NO_ITER: isize = -1;
+pub const MP_MACHEP0: f64 = 2.2204460e-16;
+
+/// Definition of MPFIT configuration structure
+pub struct MPConfig {
+    /// Relative chi-square convergence criterion  Default: 1e-10
+    pub ftol: f64,
+    /// Relative parameter convergence criterion   Default: 1e-10
+    pub xtol: f64,
+    /// Orthogonality convergence criterion        Default: 1e-10
+    pub gtol: f64,
+    /// Finite derivative step size                Default: MP_MACHEP0
+    pub epsfcn: f64,
+    /// Initial step bound                         Default: 100.0
+    pub step_factor: f64,
+    /// Range tolerance for covariance calculation Default: 1e-14
+    pub covtol: f64,
+    /// Maximum number of iterations.  If maxiter == MP_NO_ITER,
+    /// then basic error checking is done, and parameter
+    /// errors/covariances are estimated based on input
+    /// parameter values, but no fitting iterations are done.
+    pub max_iter: isize,
+    /// Maximum number of function evaluations, or 0 for no limit
+    /// Default: 0 (no limit)
+    pub max_fev: usize,
+    /// Default: true
+    pub n_print: bool,
+    /// Scale variables by user values?
+    /// true = yes, user scale values in diag;
+    /// false = no, variables scaled internally (Default)
+    pub do_user_scale: bool,
+    /// Disable check for infinite quantities from user?
+    /// true = perform check
+    /// false = do not perform check (Default)
+    pub no_finite_check: bool,
+}
+
+impl ::std::default::Default for MPConfig {
+    fn default() -> Self {
+        MPConfig {
+            ftol: 1e-10,
+            xtol: 1e-10,
+            gtol: 1e-10,
+            epsfcn: MP_MACHEP0,
+            step_factor: 100.0,
+            covtol: 1e-14,
+            max_iter: MP_NO_ITER,
+            max_fev: 0,
+            n_print: true,
+            do_user_scale: false,
+            no_finite_check: false,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
